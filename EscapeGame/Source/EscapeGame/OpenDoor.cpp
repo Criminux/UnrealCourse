c3a699a1a58e88be11c3838f335f975ac05c923d
Sebastian <sebastian@michael-bos.de>
2017-06-29 13:01:20 +0200

    Built first room

---
+++ EscapeGame/Source/EscapeGame/OpenDoor.cpp
@@ -0,0 +1,38 @@
+// Copyright Sebastian Bos 2017
+
+#include "OpenDoor.h"
+#include "Gameframework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AActor *Owner = GetOwner();
+	
+	FRotator doorRotation = FRotator(0.0f, 70.0f, 0.0f);
+
+	Owner->SetActorRotation(doorRotation);
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+

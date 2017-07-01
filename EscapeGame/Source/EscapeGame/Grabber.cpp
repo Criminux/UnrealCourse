fbd7c2792d642c144d3d7647873ca2ba26b77a72
Sebastian <sebastian@michael-bos.de>
2017-07-01 14:18:27 +0200

    Added Grabbervisualization

--- EscapeGame/Source/EscapeGame/Grabber.cpp
+++ EscapeGame/Source/EscapeGame/Grabber.cpp
@@ -1,7 +1,11 @@
 // Copyright Sebastian Bos 2017
 
 #include "Grabber.h"
+#include "GameFramework/Actor.h"
+#include "DrawDebugHelpers.h"
 
+
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -29,6 +33,30 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation
+	);
+
+	/* LOGGING
+	UE_LOG(LogTemp, Warning, TEXT("PlayerLocation: %s  PlayerRotation: %s"), 
+		*PlayerViewPointLocation.ToString(), 
+		*PlayerViewPointRotation.ToString()
+	)*/
+
+	FVector LineTraceEnd = PlayerViewPointLocation + (PlayerViewPointRotation.Vector() * Reach);
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		0.0f,
+		0.0f,
+		10.0f
+	);
+
 }
 
